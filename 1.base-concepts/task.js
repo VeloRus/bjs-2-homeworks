3a5b809a194b56084113023ca2a4982007f5ebf0
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-07-03 21:07:37 +0300

    homework without task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,34 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+//Задача 1
+"use strict"
+
+function solveEquation (a, b, c) {
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  let x1, x2;
+  
+  if (d < 0) {
+      
+  } else if (d == 0) {
+     x1 = -b / (2 * a);
+     arr[0] = x1
+        
+  } else if (d > 0) {
+
+    x1 = (-b + Math.sqrt(d) )/(2*a);
+    x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr[0] = x1;
+    arr[1] = x2;
+  }
+  return(arr);
 }
+
+
+
+
+//Задача 2
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
 
   return totalAmount;
 }
