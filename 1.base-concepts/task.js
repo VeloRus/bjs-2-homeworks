60d4bcdb5cc8c009aafe3f58769f771ba5cc82ab
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-07-05 22:46:19 +0300

    added task №2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -27,8 +27,39 @@ function solveEquation (a, b, c) {
 
 //Задача 2
 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  return totalAmount;
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  }
+  if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+  }
+  if (isNaN(amount)) {
+      return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
+  }
+ 
+let S =  amount - contribution;
+
+let nowDate = new Date();
+let currentYear = nowDate.getFullYear();
+let endYear = date.getFullYear();
+let currentMonth = nowDate.getMonth();
+let endMonth = date.getMonth();
+
+ if (currentMonth === 0) {
+    currentMonth++;
+    endMonth++;
+  }
+let n = (endYear - currentYear) * 12 + (endMonth - currentMonth);
+let P = percent / 100 / 12;
+
+let monthlyPayment = S * (P + (P / (((1 + P) ** n) - 1)));
+
+  totalAmount = monthlyPayment * n;
+  totalAmount=Number(totalAmount.toFixed(2));
+
+   return(totalAmount);
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,12 +1,26 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
-}
-
+  
+    min = -Infinity;
+    max = Infinity;
+    sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+      
+      min = Math.min(...arr)
+      max = Math.max(...arr)
+      
+      sum += arr[i];    
+      let interm = sum / arr.length;
+      avg = +interm.toFixed(2);
+    }
+    
+  
+    return{ min: min, max: max, avg: avg };
+  }
+  
+  
 // Задание 2
 function worker(arr) {
   let sum;
