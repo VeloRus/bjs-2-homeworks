7852a604092904a7e62a62ef41138877f1e73a80
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-07-06 22:37:47 +0300

    arrays homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
+//Задача 1
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
+  
   return result; // boolean
 }
 
+
+//задача 2
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let positiveNumbers = arr.filter(item => item > 0 && item % 3 === 0);
+  
+  resultArr = positiveNumbers.map(item => item * 10);
+  
+  return resultArr;
 }
