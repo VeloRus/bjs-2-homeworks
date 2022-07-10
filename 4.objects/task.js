ee34f10cff11ed51d9c8c909e02c3ddfcad78a06
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-07-10 17:51:42 +0300

    objects homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+ this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+if(this.marks === undefined){ 
+      this.marks = [mark];
+  } else { 
+  this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks =  function(...mark) {
+if (this.marks === undefined) { 
+this.marks = [mark];
+} else {
+  this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+let sum = 0;
+this.marks.forEach(item => {sum += item});
+return sum / this.marks.length
+}
+
+Student.prototype.exclude = function(reason) {
+this.exclude = reason;
+
+delete this.subject;
+delete this.marks;
+}
+
+let student2 = new Student("Buzz", "female", 35);
+student2.setSubject("Geometry");
+student2.addMark(2);
+student2.addMark(3);
+student2.addMark(2);
+student2.exclude('low grades')
+console.log(student2)
